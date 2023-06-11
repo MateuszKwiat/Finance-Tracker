11600b048c25106879bb0ab567f803d76845932a
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2023-06-11 02:28:54 +0200

    initial commit

---
+++ FinanceTracker/FinanceTracker/src/test/java/com/financeTracker/FinanceTracker/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeTracker.FinanceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
